0f6badbb4704b007a70dc373dddc55b907406a50
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-30 19:28:56 +0000

    add task update, task delete and task form

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from  .views import TaskList, TaskDetail, TaskCreate
+from  .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
 
 urlpatterns = [
     path('', TaskList.as_view(), name="task"),
     path('task-create/', TaskCreate.as_view(), name="task-create"),
+    path('task-update/', TaskUpdate.as_view(), name='task-update'),
+    path('task-delete/', DeleteView.as_view(), name='task-delete'),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django .views.generic.edit import CreateView
+from django .views.generic.edit import UpdateView
+from django .views.generic.edit import DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -16,7 +18,20 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
 
+
 class TaskCreate(CreateView):
     model = Task
     fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('task')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('task')
+
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
     success_url = reverse_lazy('task')
