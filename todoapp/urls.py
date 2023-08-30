5b8208fe47739b5d44ffbdc87873fe94f328a2c8
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-30 18:50:06 +0000

    add create task funtions and save

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -3,5 +3,5 @@ from  .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
     path('', TaskList.as_view(), name="task"),
-    path('task-creat/', TaskCreate.as_view(), name="task-create"),
+    path('task-create/', TaskCreate.as_view(), name="task-create"),
 ]
