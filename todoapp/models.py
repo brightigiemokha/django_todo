b9be683dcc86c5d96f5db4dddc684b12baa325d4
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-30 13:27:41 +0000

    add super user and task. add migration

--- todoapp/models.py
+++ todoapp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Task(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, black=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=300, null=True)
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
