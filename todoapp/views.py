bbcd55fc92f9d0876ae511a29302b3bcac831a32
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-30 17:21:39 +0000

    add task details

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Task
 
@@ -8,3 +9,7 @@ from .models import Task
 class TaskList(ListView):
     model = Task
     context_object_name = 'task'
+
+
+class TaskDetail(DetailView):
+    model = Task
