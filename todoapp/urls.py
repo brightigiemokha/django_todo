cd098c56ef67496343f96c9d46ded8a85de81003
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-31 09:38:55 +0000

    add logout function

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TaskList.as_view(), name="task"),
     path('login/', CustomLoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(), name="logout"),
     path('task-create/', TaskCreate.as_view(), name="todo-create"),
     path('task/<int:pk>/', TaskDetail.as_view(), name="todo-detail"),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name="todo-update"),
