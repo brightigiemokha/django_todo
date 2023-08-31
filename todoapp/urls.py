2401cd4cc9c74a4e5ef6b3f6312b3539aa0993b5
brightigiemokha <brightigiemokha@yahoo.com>
2023-08-31 11:38:44 +0000

    add kwarg restriction from already registered user from been able to access signup page

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, SignupPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TaskList.as_view(), name="task"),
     path('login/', CustomLoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(next_page='login'), name="logout"),
+    path('signup', SignupPage.as_view(), name="signup"),
     path('task-create/', TaskCreate.as_view(), name="todo-create"),
     path('task/<int:pk>/', TaskDetail.as_view(), name="todo-detail"),
     path('task-update/<int:pk>/', TaskUpdate.as_view(), name="todo-update"),
